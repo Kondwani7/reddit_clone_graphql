3484d77dbeedb0fbb4733e27072cb3d579cbe4ba
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-02-17 22:45:46 +0200

    search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { HiOutlineChatBubbleOvalLeftEllipsis } from 'react-icons/hi2'
 import { BsArrowUpRightCircle, BsBell, BsChatLeftText } from 'react-icons/bs'
-import { BiChevronDown } from 'react-icons/bi'
+import { BiChevronDown,BiSearch } from 'react-icons/bi'
 import { TfiAnnouncement } from 'react-icons/tfi'
 import { RiCoinsLine } from 'react-icons/ri'
 import { AiOutlinePlus } from 'react-icons/ai'
@@ -23,6 +23,16 @@ function Header() {
                 <p className='text-base font-medium mt-[6px] ml-2 hidden flex-1 lg:inline'>Home</p>
                 <BiChevronDown size={25} />
             </div>
+            {/*search box */}
+            <form className='ml-6 h-12 flex flex-1 items-center space-x-2 rounded-full border-gray-200 bg-gray-100 px-3 py-1'>
+                <BiSearch className='text-gray-400' size={25}/>
+                <input 
+                    className='flex-1 bg-transparent outline-none'
+                    type="text"
+                    placeholder='Search Reddit'
+                />
+                <button type='submit' hidden/>
+            </form>
         </div>
     )
 }
