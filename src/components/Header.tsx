96d68bac0010e1a93b84ad9cfcc25f640b54df21
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-02-17 21:49:14 +0200

    company logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
+import {HiOutlineChatBubbleOvalLeftEllipsis} from 'react-icons/hi2'
+import {BsArrowUpRightCircle, BsBell, BsChatLeftText} from 'react-icons/bs'
+import {BiChevronDown} from 'react-icons/bi'
+import {TfiAnnouncement} from 'react-icons/tfi'
+import {RiCoinsLine} from 'react-icons/ri'
+import {AiOutlinePlus} from 'react-icons/ai'
 
 function Header() {
   return (
     <div>
-      Header
+      <div className='relative h-[50px] w-20 flex-shrink-0 cursor-pointer'>
+        <Image
+            objectFit='contain'
+            layout="fill"
+            src="/img/reddit_logo.png"
+            alt="profile image"
+        />
+      </div>
     </div>
   )
 }
