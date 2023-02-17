ce1386eb7b84d8b1bf26e24bd6a749a3b6cfb9bd
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-02-18 01:22:44 +0200

    final next auth and styling Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,11 @@ import { TfiAnnouncement } from 'react-icons/tfi'
 import { RiCoinsLine } from 'react-icons/ri'
 import { AiOutlinePlus } from 'react-icons/ai'
 import { HiHome } from 'react-icons/hi'
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 function Header() {
+    const { data: session } = useSession()
     return (
-        <div className='flex bg-white px-3 py-2'>
+        <div className='flex bg-white px-3 py-2 h-16'>
             <div className='-mt-6 relative h-24 w-24 flex-shrink-0 cursor-pointer'>
                 <Image
                     objectFit='contain'
@@ -19,7 +20,7 @@ function Header() {
                     alt="profile image"
                 />
             </div>
-            <div className='ml-6 -mt-6 flex items-center xl:min-w-[300px]'>
+            <div className='ml-6  flex items-center xl:min-w-[300px]'>
                 <HiHome size={25} />
                 <p className='text-base font-medium mt-[6px] ml-2 hidden flex-1 lg:inline'>Home</p>
                 <BiChevronDown size={25} />
@@ -46,7 +47,7 @@ function Header() {
                 <button
                     className=' flex items-center mt-1 text-gray-900 font-semibold 
                     h-10 py-2 px-4 bg-gray-300 hover:bg-gray-500 rounded-2xl'>
-                    <TfiAnnouncement 
+                    <TfiAnnouncement
                         className='h-8 w-6 lg:w-12 cursor-pointer rounded-sm lg:p-1
                       lg:hover:bg-gray-100'/>
                     <p className='font-normal'>Advertise</p>
@@ -57,25 +58,35 @@ function Header() {
                 <BsBell className='icon' />
                 <AiOutlinePlus className='icon' />
             </div>
-            {/*sign in*/}
-            <div onClick={() =>signIn()} className='ml-2 flex'>
-                <button 
-                    className='items-center mt-1 text-white w-32 font-semibold
-                    h-10 py-2 px-4 bg-blue-700 hover:bg-blue-500 rounded-2xl'>
-                    Log In
-                </button>
-            </div>
-            {/*sign out*/}
-            <div className='hidden ml-8 -mt-6 flex items-center xl:min-w-[200px]'>
-                <Image
-                    width={41}
-                    height={41}
-                    src="/img/profile_image2.png"
-                    alt="profile image"
-                />
-                <p className='text-base font-medium mt-[6px] ml-2 hidden flex-1 lg:inline'>Home</p>
-                <BiChevronDown size={25} />
-            </div>
+            {session ? (
+                <div onClick={() => signOut()} className=' cursor-pointer ml-8 flex items-center xl:min-w-[300px]'>
+                    <Image
+                        width={41}
+                        height={41}
+                        src="/img/profile_image2.png"
+                        alt="profile image"
+                    />
+                    <div className='flex-1 text-xs'>
+                        <p className='ml-2 font-medium truncate'>{session?.user?.name}</p>
+                        <p className='font-medium text-gray-400 mt-[6px] ml-2 lg:inline'>1 Karma</p>
+                    </div>
+
+                    <BiChevronDown size={25} />
+                </div>
+
+            ) : (
+                <div onClick={() => signIn()} className='ml-2 flex'>
+                    <button
+                        className='items-center mt-1 text-white w-32 font-semibold
+                        h-10 py-2 px-4 bg-blue-700 hover:bg-blue-500 rounded-2xl'>
+                        Log In
+                    </button>
+                </div>
+            )
+
+            }
+
+
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <Header/>
+        
       </main>
     </>
   )
