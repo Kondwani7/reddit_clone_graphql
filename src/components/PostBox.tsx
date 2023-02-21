85322e70e62d4700a17d6c0f30422dfc54f4b023
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-02-21 17:18:04 +0200

    form for posting title body subreddit and image url

--- src/components/PostBox.tsx
+++ src/components/PostBox.tsx
@@ -1,27 +1,91 @@
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useState } from 'react'
 import Avatar from './Avatar'
-import {CiImageOn} from 'react-icons/ci'
-import {BsLink45Deg} from 'react-icons/bs'
+import { CiImageOn } from 'react-icons/ci'
+import { BsLink45Deg } from 'react-icons/bs'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  postTitle: string
+  postBody: string
+  postImage: string
+  subreddit: string
+}
+
 function PostBox() {
-    const {data: session} = useSession()
+  const { data: session } = useSession()
+  const [imageBoxOpen, setImageBoxOpen] = useState(false)
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>()
+
   return (
     <form className='mt-6 sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2'>
       <div className='flex items-center space-x-3'>
         {/*Avatar */}
-        <Avatar/>
+        <Avatar />
         <input
-            disabled={!session}
-            className='flex-1 rounded-md bg-gray-50 p-2 outline-none'
-            type='text'
-            placeholder={
-                session ? 'Create a new post': 'log in to post'
-            }
+          {...register('postTitle', { required: true })}
+          disabled={!session}
+          className='flex-1 rounded-md bg-gray-50 p-2 outline-none'
+          type='text'
+          placeholder={
+            session ? 'Create a new post' : 'log in to post'
+          }
         />
 
-        <CiImageOn size={25} className='cursor pointer text-gray-500'/>
-        <BsLink45Deg size={25} className='text-gray-500 cursor pointer'/>
+        <CiImageOn 
+          onClick={() => setImageBoxOpen(!imageBoxOpen)} 
+          size={25} 
+          className={`cursor-pointer text-gray-500 ${
+            imageBoxOpen && 'text-blue-300'
+          }`}
+        />
+        <BsLink45Deg size={25} className='text-gray-500 cursor-pointer' />
       </div>
+      {/*create post */}
+      {watch('postTitle') && (
+        <div className='flex flex-col py-2'>
+          {/*body*/}
+          <div className='flex items-center px-2'>
+            <p className='min-w-[90px]'>Body:</p>
+            <input
+              className='m-2 flex-1 bg-blue-50 p-2 outline-none'
+              {...register('postBody')}
+              type='text'
+              placeholder='Text (optional)'
+            />
+          </div>
+          {/*subreddit*/}
+          <div className='flex items-center px-2'>
+            <p className='min-w-[90px]'>Subdreddit:</p>
+            <input
+              className='m-2 flex-1 bg-blue-50 p-2 outline-none'
+              {...register('subreddit')}
+              type='text'
+              placeholder='i.e sports'
+            />
+          </div>
+          {/*image url*/}
+          {imageBoxOpen && (
+            <div className='flex items-center px-2'>
+              <p className='min-w-[90px]'>Image URL:</p>
+              <input
+                className='m-2 flex-1 bg-blue-50 p-2 outline-none'
+                {...register('postImage')}
+                type='text'
+                placeholder='Optional...'
+              />
+            </div>
+          )}
+
+        </div>
+      )}
     </form>
   )
 }
